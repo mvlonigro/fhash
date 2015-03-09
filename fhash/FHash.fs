a30079949b1a5e0e9fd20afd985b88aaa7f13983
Michael LoNigro <mvlonigro@users.noreply.github.com>
2015-03-09 16:59:25 -0600

    Improved comments for F# modules

--- fhash/FHash.fs
+++ fhash/FHash.fs
@@ -25,6 +25,7 @@ open System.Security.Cryptography
 
 /// Some utility functions used in this library
 module internal CommonLibrary =
+    /// Times the execution of a function and prints it to stdout
     let time f =
         let timer = new Stopwatch()
         timer.Start()
@@ -32,11 +33,13 @@ module internal CommonLibrary =
         printfn "\nElapsed Time: %f" timer.Elapsed.TotalMilliseconds
         returnValue
 
+    /// Converts a byte array into a string representation of a hex value
     let convertBytesToHex (bytes:byte[]) =
         bytes
         |> Seq.map( fun c -> c.ToString("X2") )
         |> Seq.reduce(+)
 
+    /// Converts a string representation of a hex value into a byte array
     let convertHexStrToBytes (hexstr:string) =
         hexstr
         |> Seq.windowed 2
@@ -45,15 +48,16 @@ module internal CommonLibrary =
         |> Seq.map( fun (_,j) -> Byte.Parse(new String(j), Globalization.NumberStyles.AllowHexSpecifier) )
         |> Seq.toArray
 
-    // Turns an internal F# record into a KeyValuePair with key: file name and value: requested signature
+    /// Turns an internal F# record into a KeyValuePair with key: file name and value: requested signature
     let convertToKeyValuePair (hRecord : DomainTypes.HashRecord) =
         new KeyValuePair<string, string>(hRecord.file, hRecord.signature)
 
-
+/// Functions relating to reading files
 module internal FileReader =
     open CommonLibrary
     open DomainTypes
 
+    /// Gets all file paths in the provided directory path + all subdirectory paths
     let rec getDirectoryContentsRec path =
         seq {
             // Get all files in this directory. yield! merges sequence of files into parent sequence
@@ -63,11 +67,13 @@ module internal FileReader =
             yield! getDirectoryContentsRec p
         }
 
+    /// Gets all file paths in the provided directory path
     let getDirectoryContents path =
         seq {
             yield! Directory.GetFiles path
         }
 
+    /// Decides whether to use recursive or non-recursive directory contents function
     let getFilesInPath path recOption =
         match recOption with
         | true ->
@@ -75,11 +81,12 @@ module internal FileReader =
         | false ->
             getDirectoryContents path
 
+    /// Resets the FileStream so signatures are consistent. Returns an int that is not needed.
     let resetStream(stream : FileStream) =
-        // Used to reset the FileStream so signatures are consistent. Returns an int that is not needed.
         stream.Seek((int64)0, SeekOrigin.Begin) |> ignore
         stream
 
+    /// Creates a .NET filestream object from a file path (with some default values)
     let createFileStream fileName =
         // Using 128Kb buffer size. Note: .NET default is 4Kb buffer size (4096)
         new FileStream (fileName, FileMode.Open, FileAccess.Read, FileShare.Read, 131072, true)
@@ -88,28 +95,33 @@ module internal FileReader =
 /// Signature algorithm types that work with FHash 
 type SignatureType = MD5=0 | SHA1=1 | SHA256=2
 
+/// Functions related to file hashing
 module internal FileHasher = 
     open CommonLibrary
     open DomainTypes
-    // Private Functions
+    
+    /// Create an MD5 signature of a filestream
     let md5Hash (input : FileStream) =
         use md5 = MD5.Create()
         input
         |> md5.ComputeHash
         |> convertBytesToHex
 
+    /// Create a SHA1 signature of a filestream
     let sha1Hash (input : FileStream) =
         use sha1 = SHA1.Create()
         input
         |> sha1.ComputeHash
         |> convertBytesToHex
 
+    /// Create a SHA256 signature of a filestream
     let sha256Hash (input : FileStream) =
         use sha256 = SHA256.Create()
         input
         |> sha256.ComputeHash
         |> convertBytesToHex
 
+    /// Choose which hashing algorithm to use
     let hashFile fileInput sigType =
         match sigType with
         | SignatureType.MD5 ->
@@ -119,6 +131,7 @@ module internal FileHasher =
         | SignatureType.SHA256 ->
             sha256Hash fileInput
 
+    /// Creates a HashRecord from a file stream
     let populateHashRecord fileInput sigType =
         {
             signature = hashFile fileInput sigType;
